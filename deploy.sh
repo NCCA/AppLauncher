79b87694bc122975225e43121aed5b36cbfd1148
jmacey <jmacey@bournemouth.ac.uk>
2025-06-18 08:57:02 +0100

    updated to add some installers

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,6 @@
     
 pyside6-rcc resources.rcc -o resources_rc.py
 uv run pyinstaller --onefile --windowed --add-data 'resources_rc.py:.' --add-data 'apps.json:.' main.py --name AppsEre
+cp dist/AppsEre /public/devel/25-26/AppsEre
+cp apps.json /public/devel/25-26/AppsEre
+cp appsereicon.png  /public/devel/25-26/AppsEre
