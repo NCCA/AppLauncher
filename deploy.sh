b135b4fc50cb6efe2b936a25bcdf84dc3d8b3482
jmacey <jmacey@bournemouth.ac.uk>
2025-06-20 09:42:37 +0100

    updated deploy to add what extra runtyime files are needed

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,10 @@ cp dist/AppsEre /public/devel/25-26/AppsEre
 cp apps.json /public/devel/25-26/AppsEre
 cp appsereicon.png  /public/devel/25-26/AppsEre
 cp AppsEre.desktop /public/devel/25-26/AppsEre
+
+# Note as I am now using QtWebEngine I need to deploy loads of extra runtime stuff as follows
+# This can be copied from the .venv 
+# AppsEre          apps.json   qtwebengine_devtools_resources.pak  qtwebengine_resources_200p.pak
+# AppsEre.desktop  icudtl.dat  QtWebEngineProcess                  qtwebengine_resources.pak
+#appsereicon.png  locales     qtwebengine_resources_100p.pak      v8_context_snapshot.bin
+# Note some of this is generated on first launch as well.
